0c028c72fa76acc2b24271c5f57ab0b9c4ce10cd
HarshSindhi1109 <harshsindhi085@gmail.com>
2025-02-07 18:13:54 +0530

    Adding NewMap.java

--- src/main/java/org/example/NewMap.java
+++ src/main/java/org/example/NewMap.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NewMap {
+    public void getNewMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("a1", "Madara Uchiha");
+        map.put("a2", "Sosuke Aizen");
+        map.put("a3", "Donquixote Doflamingo");
+        map.put("a4", "Ryomen Sukuna");
+
+        System.out.println(map);
+    }
 }
